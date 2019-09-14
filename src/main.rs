c309a8b46ee5bdc9149e9b2dc9f6742c903baa56
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-09-15 00:04:18 +0200

    game finished

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,31 @@ fn main() {
     println!("Guess the nummer!");
     println!("Input your guess!");
 
-    let secret_number: i64 = rand::thread_rng().gen_range(1, 101);
-
-    println!("Random number: {}", secret_number);
-
-    let mut guess = String::new();
-
-    match stdin().read_line(&mut guess) {
-        Ok(_n) => {
-            println!("{}", guess.len());
-        }
-        Err(error) => println!("error: {}", error)
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
+    println!("{}", secret_number);
+    
+    let mut did_guess: bool = false;
+    
+    while !did_guess {
+        let mut guess = String::new();
+        match stdin().read_line(&mut guess) {
+            Ok(_n) => {
+                let s = String::from(secret_number.to_string());
+                if are_two_strings_equal(guess, s) {
+                    did_guess = true;
+                }
+                
+            }
+            Err(error) => println!("error: {}", error)
+        };
     }
 
-    println!("You guessed: {}", guess);
+    fn are_two_strings_equal(str1: String, str2: String) -> bool {
+        // Slice the trailing space...
+        let trimmed1: i32 = str1.trim().parse::<i32>().unwrap();
+        let trimmed2: i32 = str2.trim().parse::<i32>().unwrap();
+        if trimmed1 > trimmed2 { println!("Try lower") } else { println!("Try higher") }
+        if trimmed1 == trimmed2 { println!("success") }
+        return trimmed1 == trimmed2;
+    }
 }
