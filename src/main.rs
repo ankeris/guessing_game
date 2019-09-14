11b31a5b04537783bc29da077fdfbee7d43fea04
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-09-14 20:18:53 +0200

    random number generator added

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use std::io::{stdin};
+use rand::Rng;
 
 fn main() {
     println!("Guess the nummer!");
     println!("Input your guess!");
 
+    let secret_number: i64 = rand::thread_rng().gen_range(1, 101);
+
+    println!("Random number: {}", secret_number);
+
     let mut guess = String::new();
 
     match stdin().read_line(&mut guess) {
-        Ok(n) => {
-            println!("{} bytes read", n);
-            println!("{}", guess);
+        Ok(_n) => {
+            println!("{}", guess.len());
         }
         Err(error) => println!("error: {}", error)
     }
