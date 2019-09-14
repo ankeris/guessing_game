9eeeff0416cb68f3151e53deeb26337e089e9f9c
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-09-14 19:35:45 +0200

    added 'rand' dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io::{stdin};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the nummer!");
+    println!("Input your guess!");
+
+    let mut guess = String::new();
+
+    match stdin().read_line(&mut guess) {
+        Ok(n) => {
+            println!("{} bytes read", n);
+            println!("{}", guess);
+        }
+        Err(error) => println!("error: {}", error)
+    }
+
+    println!("You guessed: {}", guess);
 }
